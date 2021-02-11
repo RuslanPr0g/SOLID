653812f600d74ff0c84a97f0c326c2804b9755cb
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-11 11:33:02 +0300

    first look at report class

---
+++ S/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/Report.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Report
+    {
+        public string Text { get; set; }
+        public void GoToFirstPage()
+        {
+            Console.WriteLine("GoToFirstPage");
+        }
+
+        public void GoToLastPage()
+        {
+            Console.WriteLine("GoToLastPage");
+        }
+
+        public void GoToPage(int pageNumber)
+        {
+            Console.WriteLine("GoToPage {0}", pageNumber);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Print report...");
+            Console.WriteLine(Text);
+        }
+    }
+}
