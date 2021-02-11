1ac228a89a7aa843be44035dbe09ac558b1a0573
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-11 11:41:41 +0300

    another example

---
+++ S/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/Store/MobileStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleResponsibilityPrinciple
+{
+    class MobileStore
+    {
+        List<Phone> phones = new List<Phone>();
+        public void Process()
+        {
+            Console.WriteLine("Enter model:");
+            string model = Console.ReadLine();
+            Console.WriteLine("Enter price:");
+            int price = 0;
+            bool result = Int32.TryParse(Console.ReadLine(), out price);
+
+            if (result == false || price <= 0 || String.IsNullOrEmpty(model))
+            {
+                throw new Exception("Wrong data.");
+            }
+            else
+            {
+                phones.Add(new Phone { Model = model, Price = price });
+                using (System.IO.StreamWriter writer = new System.IO.StreamWriter("store.txt", true))
+                {
+                    //writer.WriteLine(model);
+                    //writer.WriteLine(price);
+                }
+                Console.WriteLine("Data was successfully proceed.");
+            }
+        }
+    }
+}
---
+++ S/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/Store/Phone.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Phone
+    {
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
