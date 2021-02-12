319e2ba1773a80419aa67a6928fb9d3bda107857
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-12 20:54:03 +0300

    Open-Closed Principle

---
+++ O/Open_Closed_Principle/Open_Closed_Principle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Closed_Principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("open-closed principle");
+        }
+    }
+}
