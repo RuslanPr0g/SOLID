6c08307485fd2b140e5ad3fd669ddddc525c508e
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-12 21:01:47 +0300

    wrong class

---
+++ O/Open_Closed_Principle/Open_Closed_Principle/Cook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Closed_Principle
+{
+    class Cook
+    {
+        public string Name { get; set; }
+
+        public Cook(string name)
+        {
+            this.Name = name;
+        }
+
+        public void MakeDinner()
+        {
+            Console.WriteLine("Clean potatoes");
+            Console.WriteLine("Boild potatoes");
+            Console.WriteLine("Drain water, de-boil boiled potatoes in mashed potatoes");
+            Console.WriteLine("Mashed with spices and greens");
+            Console.WriteLine("Mashed Potatoes Ready");
+        }
+    }
+}
