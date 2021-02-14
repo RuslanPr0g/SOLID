5e5ded94bdcbb3bb485db903ad72e4a9b3c09777
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-14 21:59:19 +0300

    wrong implementation

--- L/LiskovSubstitution/LiskovSubstitution/Program.cs
+++ L/LiskovSubstitution/LiskovSubstitution/Program.cs
@@ -14,10 +14,19 @@ namespace LiskovSubstitution
 
         public static void TestRectangleArea(Rectangle rect)
         {
-            rect.Height = 5;
-            rect.Width = 10;
-            if (rect.GetArea() != 50)
-                throw new Exception("Wrong area!");
+            if (rect is Square)
+            {
+                rect.Height = 5;
+                if (rect.GetArea() != 25)
+                    throw new Exception("Wrong area!");
+            }
+            else if (rect is Rectangle)
+            {
+                rect.Height = 5;
+                rect.Width = 10;
+                if (rect.GetArea() != 50)
+                    throw new Exception("Wrong area!");
+            }
         }
     }
 }
